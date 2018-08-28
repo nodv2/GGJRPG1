5d1879cf904d27df84aae7a40ace892e5123b0b9
Grant Jackson <ultimateggj@gmail.com>
2018-08-28 14:31:14 -0400

    Added Location class and locations

--- GGJRPG1/Engine/ViewModels/GameSession.cs
+++ GGJRPG1/Engine/ViewModels/GameSession.cs
@@ -11,6 +11,8 @@ namespace Engine.ViewModels
     {
         public Player CurrentPlayer { get; set; }
 
+        public Location CurrentLocation { get; set; }
+
         public GameSession()
         {
             CurrentPlayer = new Player();
@@ -20,6 +22,13 @@ namespace Engine.ViewModels
             CurrentPlayer.Gold = 100000;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "This is your house";
+            CurrentLocation.ImageName = "/Engine;component/Images/Locations/Home.png";
         }
     }
 }
