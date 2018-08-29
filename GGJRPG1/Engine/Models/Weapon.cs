cdf9da8b685a803dcd67d2f0c8a4ffd8118f8c80
Grant Jackson <ultimateggj@gmail.com>
2018-08-29 14:44:07 -0400

    More Refactoring

--- GGJRPG1/Engine/Models/Weapon.cs
+++ GGJRPG1/Engine/Models/Weapon.cs
@@ -12,7 +12,7 @@ namespace Engine.Models
         public int MaximumDamage { get; set; }
 
         public Weapon(int itemTypeID, string name, int price, int minDamage, int maxDamage)
-            : base(itemTypeID, name, price)
+            : base(itemTypeID, name, price, true)
         {
             MinimumDamage = minDamage;
             MaximumDamage = maxDamage;
